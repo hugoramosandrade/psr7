50814662092736170593ea784ee2a1771f5126a9
Hugo Andrade <hugoramosandrade@gmail.com>
2024-05-16 08:06:28 -0300

    Implementacao da RequestInterface

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -35,6 +35,59 @@ class Request extends Message implements RequestInterface
         }
     }
 
+    public function getRequestTarget(): string
+    {
+        if ($this->requestTarget !== null) return $this->requestTarget;
+
+        if ('' === $target = $this->uri->getPath()) $target = '/';
+
+        if ($this->uri->getQuery() !== '') $target .= '?' . $this->uri->getQuery();
+
+        return $target;
+    }
+
+    public function withRequestTarget(string $requestTarget): RequestInterface
+    {
+        if (preg_match('#\s#', $requestTarget))
+            throw new \InvalidArgumentException("Invalid request target provided. Cannot contain whitespace");
+
+        $new = clone $this;
+        $new->requestTarget = $requestTarget;
+
+        return $new;
+    }
+
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    public function withMethod(string $method): RequestInterface
+    {
+        $new = clone $this;
+        $new->method = $method;
+
+        return $new;
+    }
+
+    public function getUri(): UriInterface
+    {
+        return $this->uri;
+    }
+
+    public function withUri(UriInterface $uri, bool $preserveHost = false): RequestInterface
+    {
+        if ($uri === $this->uri) return $this;
+
+        $new = clone $this;
+        $new->uri = $uri;
+
+        if (!$preserveHost || !$this->hasHeader('Host'))
+            $new->updateHostFromUri();
+
+        return $new;
+    }
+
     private function updateHostFromUri(): void
     {
         if ('' === $host = $this->uri->getHost()) {
