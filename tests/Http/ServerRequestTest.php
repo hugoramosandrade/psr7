4d5623b19d8eb65d94d197bdb5ead4f75b1e8c73
Hugo Andrade <hugoramosandrade@gmail.com>
2024-05-22 09:51:48 -0300

    Testes da implementacao da interface ServerRequestInterface

--- tests/Http/ServerRequestTest.php
+++ tests/Http/ServerRequestTest.php
@@ -5,6 +5,7 @@ namespace Testes\Http;
 use Psr\Http\Message\{ServerRequestInterface, UploadedFileInterface};
 use Hugo\Psr7\Http\{ServerRequest, UploadedFile};
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class ServerRequestTest extends TestCase
 {
@@ -26,11 +27,11 @@ class ServerRequestTest extends TestCase
     protected function setUp(): void
     {
         $this->uploadedFiles = [
-            new UploadedFile(__DIR__."../file/teste.csv", 1, 0, "teste.csv", "text/csv")
+            new UploadedFile(__DIR__."/../file/teste.csv", 1, 0, "teste.csv", "text/csv")
         ];
         $this->request = new ServerRequest(
             'POST',
-            'http://localhost:3001/',
+            'http://localhost:3001/?msg=teste',
             $this->headers,
             json_encode($this->stream),
             '1.1',
@@ -38,6 +39,9 @@ class ServerRequestTest extends TestCase
             $this->uploadedFiles,
             $this->cookieParams
         );
+
+        $request = $this->request->withAttribute('Teste', 'valor do atributo');
+        $this->request = $request->withAttribute('Teste2', 'valor2');
     }
 
     public function testGetServerParamsDeveRetornarArray()
@@ -69,5 +73,87 @@ class ServerRequestTest extends TestCase
         $requestNovo = $this->request->withCookieParams(['Cookie_3' => 'valor_3']);
 
         self::assertNotEquals($requestNovo, $this->request);
+    }
+
+    public function testGetQueryParamsDeveRetornarUmArray()
+    {
+        $queryParams = $this->request->getQueryParams();
+
+        self::assertIsArray($queryParams);
+        self::assertArrayHasKey('msg', $queryParams);
+        self::assertEquals('teste', $queryParams['msg']);
+    }
+
+    public function testWithQueryParamsDeveRetornarUmaNovaInstancia()
+    {
+        $requestNovo = $this->request->withQueryParams(['msg' => 'teste2']);
+
+        self::assertNotEquals($requestNovo, $this->request);
+        self::arrayHasKey('msg', $requestNovo->getQueryParams());
+        self::assertEquals('teste2', $requestNovo->getQueryParams()['msg']);
+    }
+
+    public function testGetUploadedFilesDeveRetornarArray()
+    {
+        $uploadedFiles = $this->request->getUploadedFiles();
+
+        self::assertIsArray($uploadedFiles);
+        self::assertInstanceOf(UploadedFileInterface::class, $uploadedFiles[0]);
+    }
+
+    public function testWithUploadedFilesDeveRetornarUmaNovaInstancia()
+    {
+        $uploadedFiles = [
+            new UploadedFile(__DIR__."/../file/teste2.csv", 1, 0, "teste.csv", "text/csv")
+        ];
+        $requestNovo = $this->request->withUploadedFiles($uploadedFiles);
+
+        self::assertNotEquals($requestNovo, $this->request);
+        self::assertInstanceOf(UploadedFileInterface::class, $requestNovo->getUploadedFiles()[0]);
+    }
+
+    public function testGetParsedBodyDeveRetornarObjeto()
+    {
+        $parsedBody = $this->request->getParsedBody();
+
+        self::assertInstanceOf(stdClass::class, $parsedBody);
+    }
+
+    public function testWithParsedBodyDeveRetornarUmaNovaInstancia()
+    {
+        $body = ["name" => "user", "email" => "user@email.com"];
+        $requestNovo = $this->request->withParsedBody($body);
+
+        self::assertNotEquals($requestNovo, $this->request);
+    }
+
+    public function testGetAttributesDeveRetornarArray()
+    {
+        $attributes = $this->request->getAttributes();
+
+        self::assertIsArray($attributes);
+        self::assertEquals('valor do atributo', $attributes['Teste']);
+    }
+
+    public function testGetAttributeDeveRetornarString()
+    {
+        $atribute = $this->request->getAttribute('Teste');
+        self::assertEquals('valor do atributo', $atribute);
+        self::assertNull($this->request->getAttribute('Teste3'));
+    }
+
+    public function testWithAttributeDeveRetornarNovaInstancia()
+    {
+        $requestNovo = $this->request->withAttribute('Novo', 'valor novo');
+
+        self::assertNotEquals($requestNovo, $this->request);
+    }
+
+    public function testWithoutAttributeDeveRetornarNovaInstancia()
+    {
+        $requestNovo = $this->request->withoutAttribute('Teste');
+
+        self::assertNotEquals($requestNovo, $this->request);
+        self::assertArrayNotHasKey('Teste', $requestNovo->getAttributes());
     }
 }
